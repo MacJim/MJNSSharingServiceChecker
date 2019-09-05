94c6749649cf60e1d916f9c39505487eefa8a9e9
Jim Shi <shiyilin2008@gmail.com>
2019-09-05 15:01:16 -0700

    The share button now sends an action upon mouse down rather than mouse up.
    This suppresses the previous warning messages in the console.

--- MJNSSharingServiceChecker/ViewController.m
+++ MJNSSharingServiceChecker/ViewController.m
@@ -14,6 +14,7 @@
     [super viewDidLoad];
 
     // Do any additional setup after loading the view.
+    [[self shareButton] sendActionOn:NSEventMaskLeftMouseDown];    // This is to prevent the warning messages from appearing in the console.
 }
 
 
